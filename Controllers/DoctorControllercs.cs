a1c06ffebaf58613b6ca36e34de967cc70768997
Mehmet Tat <mexmettat@gmail.com>
2025-05-08 22:30:04 +0300

    doctorpanel appointment ve patient kısmı tasarımı

--- Controllers/DoctorControllercs.cs
+++ Controllers/DoctorControllercs.cs
@@ -14,5 +14,33 @@ namespace PatientTrackingSite.Controllers
             return View();
         }
 
+        public IActionResult MyPatients()
+        {
+            // Geçici dummy veriler (ileride DB'den gelecek)
+            var patients = new List<string>
+            {
+                "Ahmet Yılmaz",
+                "Elif Kaya",
+                "Mehmet Güneş"
+            };
+
+            ViewBag.Patients = patients;
+
+            return View();
+        }
+
+        public IActionResult Appointments()
+        {
+            var appointments = new List<dynamic>
+    {
+        new { Patient = "Ahmet Yılmaz", Date = "08.05.2025", Time = "10:00", Status = "Onaylı" },
+        new { Patient = "Elif Kaya", Date = "08.05.2025", Time = "11:30", Status = "Bekliyor" },
+        new { Patient = "Mehmet Güneş", Date = "08.05.2025", Time = "14:00", Status = "İptal" }
+    };
+
+            ViewBag.Appointments = appointments;
+            return View();
+        }
+
     }
 }
