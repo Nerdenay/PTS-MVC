e98a47678ffd7c71261dc17db222f2a6851ee970
Nebi Erdenay ÇUBUKÇU <erdenaycbkc@outlook.com>
2025-05-09 14:14:55 +0300

    Db Setup and Migration Testing
    appsettings.json kısmındaki connection stringi kendi db serverinıza göre güncelleyin geri kalan ayarları değiştirmeye gerek yok, migrationlar otomatik geleceği için sadece update-db ile migrationlar EF ile DB'e yansıyacak, sadece sakın migration push etmetin tüm DB değişiklerini ben yapacağım.

---
+++ Models/PTSDBContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PatientTrackingSite.Models
+
+{
+    public class PTSDBContext : DbContext
+
+    {
+
+        public PTSDBContext(DbContextOptions<PTSDBContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Deneme> Deneme { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using PatientTrackingSite.Models;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+// Database Context configuration
+
+builder.Services.AddDbContext<PTSDBContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon")); });
+
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
